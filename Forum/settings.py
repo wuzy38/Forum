dbf62f527ab4807904d222d7a8fdcbcaac090ba0
wuzy38 <843404313@qq.com>
2018-12-27 20:11:29 +0800

    test

--- Forum/settings.py
+++ Forum/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE':'django.db.backends.mysql',
-        'NAME':'forum',      # 数据库的名称
+        'NAME':'sales',      # 数据库的名称
         'USER':'root',
         'PASSWORD':'',
         'HOST':'127.0.0.1',
